5de7a3c09ad3fec3a429743866486f15bede5b9d
LieutenantHawkeye <hwill12345@gmail.com>
2015-12-11 17:03:52 -0500

    finished problem 2

---
+++ Problem2.py
@@ -0,0 +1,18 @@
+def fibonacci(maxVal):
+	fib = []
+	x = 1
+	y = 2
+	while(y<maxVal):
+		fib.extend([x,y])
+		x = x + y
+		y = y + x
+	return fib
+
+def evenFib(maxVal):
+	s = []
+	for i in fibonacci(maxVal):
+		if i % 2 == 0:
+			s.append(i)
+	print sum(s)
+
+evenFib(4000000)
