a3be8c0c78b4d14cfb5e4064312612d79e8fc05a
LieutenantHawkeye <hwill12345@gmail.com>
2015-12-14 02:25:18 -0500

    solved problems 3 through 6
    cheated a tiny bit

---
+++ Problem3.py
@@ -0,0 +1,11 @@
+import math
+
+def largestPrimeFactor(n=600851475143):
+	for i in range(2, int(math.sqrt(n)+1)):
+		while n % i == 0:
+			n //= i
+			print("Yay, %d is a factor, now we should test %d" % (i, n))
+			if n == 1 or n == i:
+				return i
+
+print largestPrimeFactor()
---
+++ Problem6.py
@@ -0,0 +1,16 @@
+def sumOfSquares(n):
+	s = 0
+	for i in range(1,n+1):
+		s += (i**2)
+	return s
+
+def squareOfSum(n):
+	s = 0
+	for i in range(1, n+1):
+		s += i
+	return s**2
+
+def sumSquareDifference(n):
+	return squareOfSum(n) - sumOfSquares(n)
+
+print sumSquareDifference(100)
