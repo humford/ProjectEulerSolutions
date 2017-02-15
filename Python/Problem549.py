aba6ed251a0fb6c6ac3fd03c932ddb2f13ef0b4a
Henry Williams <hwill12345@gmail.com>
2017-02-15 15:39:47 -0500

    started 549

---
+++ Python/Problem549.py
@@ -0,0 +1,14 @@
+import math
+
+def s(i):
+	for m in range(1, i + 1):
+		if math.factorial(m) % i == 0:
+			return m
+
+def S(n):
+	z = 0
+	for i in range(2, n + 1):
+		z += s(i)
+	return z
+
+print S(10 ** 8)
