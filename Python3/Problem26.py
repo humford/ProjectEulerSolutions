09ae070f63cff6c3ed679aebee3d7c0fc5b87fdc
Henry Williams <hwill12345@gmail.com>
2017-05-21 02:47:34 -0400

    solve 26

--- Python3/Problem26.py
+++ Python3/Problem26.py
@@ -1,8 +1,22 @@
-def reciprocalCycle(d):
-    r = []
-    for x in range(2, d + 1):
-        r.append(float(1 / x))
-    return r
+sequencelength = 0
+num = 0
 
+for i in range(1000, 1, -1):
+    if sequencelength >= i:
+    	break
 
-print(reciprocalCycle(1000))
+    foundRemainders = [0 for x in range(i)]
+    value = 1
+    position = 0
+
+    while foundRemainders[value] == 0 and value != 0:
+    	foundRemainders[value] = position
+    	value *= 10
+    	value %= i
+    	position += 1
+
+    if position - foundRemainders[value] > sequencelength:
+    	num = i
+    	sequencelength = position - foundRemainders[value]
+
+print(num, sequencelength)
