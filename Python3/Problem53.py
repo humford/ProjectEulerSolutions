d3b7ff742daa819f29aefd05c755cb6bd20b458d
Henry Williams <hwill12345@gmail.com>
2017-07-17 11:53:43 -0400

    fix 53

--- Python3/Problem53.py
+++ Python3/Problem53.py
@@ -1,8 +1,13 @@
-from gmpy2 import comb
+import math
+
+def nCr(n,r):
+    f = math.factorial
+    return f(n) / f(r) / f(n-r)
+
 count = 0
 for n in range(1, 101):
     for r in range(1, n):
-        c = comb(n,r)
+        c = nCr(n,r)
         if c > 1000000:
             count += 1
 print(count)
