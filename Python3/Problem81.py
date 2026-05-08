0de2b1a943d572a0af93d4e75f381e8a7d852d0b
Henry Williams <hwill12345@gmail.com>
2026-05-08 15:33:34 -0700

    solved Problem 81

--- Python3/Problem81.py
+++ Python3/Problem81.py
@@ -1,18 +1,49 @@
-exampleMatrix = [[131, 673, 234, 103, 18], [201, 96, 342, 965, 150], [630, 803, 746, 422, 111],
-                 [537, 699, 497, 121, 956], [805, 732, 524, 37, 331]]
-
-matrix = open('../Files/p081_matrix.txt', 'r')
-
-def shortestPathBruteForce(m):
-    paths = []
-    var = m[0][0]
+import time
+from pathlib import Path
 
 
-def shortestPath(m):
-    pass
+def readMatrix():
+    path = Path(__file__).resolve().parents[1] / "Files" / "p081_matrix.txt"
+    return [
+        [int(value) for value in line.split(",")]
+        for line in path.read_text().strip().splitlines()
+    ]
 
-start = time.time()
-answer = powerfuldigitcounts()
-elapsed = (time.time() - start)
 
-print("Found " + str(answer) + " in " + str(elapsed) + " seconds.")
+def minimalPathSum(matrix):
+    totals = [row[:] for row in matrix]
+
+    for row in range(len(totals)):
+        for col in range(len(totals[row])):
+            if row == 0 and col == 0:
+                continue
+
+            options = []
+            if row > 0:
+                options.append(totals[row - 1][col])
+            if col > 0:
+                options.append(totals[row][col - 1])
+
+            totals[row][col] += min(options)
+
+    return totals[-1][-1]
+
+
+def runTests():
+    example = [
+        [131, 673, 234, 103, 18],
+        [201, 96, 342, 965, 150],
+        [630, 803, 746, 422, 111],
+        [537, 699, 497, 121, 956],
+        [805, 732, 524, 37, 331],
+    ]
+    assert minimalPathSum(example) == 2427
+
+
+if __name__ == "__main__":
+    runTests()
+    start = time.time()
+    answer = minimalPathSum(readMatrix())
+    elapsed = time.time() - start
+
+    print("Found " + str(answer) + " in " + str(elapsed) + " seconds.")
