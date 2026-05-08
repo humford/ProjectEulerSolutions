cb7f6cad043be57288a53ee345df12e122a2fc2f
Henry Williams <hwill12345@gmail.com>
2026-05-08 15:33:33 -0700

    solved Problem 76

--- Python3/Problem76.py
+++ Python3/Problem76.py
@@ -1,18 +1,25 @@
 import time
-import math
 
-#how many different way can a number be written
-#as the sum of at least two positive integers
-def count_sums(n):
-    count = 0
-    for i in range(n, 0, -1):
-        for j in range(1, i+1):
-            if i + j == n:
-                count += count_sums(i) + count_sums(j)
-    return count
 
-start = time.time()
-answer = count_sums(100)
-elapsed = (time.time() - start)
+def countSums(n):
+    ways = [0] * (n + 1)
+    ways[0] = 1
 
-print("Found " + str(answer) + " in " + str(elapsed) + " seconds.")
+    for addend in range(1, n):
+        for total in range(addend, n + 1):
+            ways[total] += ways[total - addend]
+
+    return ways[n]
+
+
+def runTests():
+    assert countSums(5) == 6
+
+
+if __name__ == "__main__":
+    runTests()
+    start = time.time()
+    answer = countSums(100)
+    elapsed = time.time() - start
+
+    print("Found " + str(answer) + " in " + str(elapsed) + " seconds.")
